0559f0f8f4cdc0803155650a7354a44e51577a30
roy71100 <aroy71010@gmail.com>
2018-12-26 01:03:58 +0200

    Add asteroids

---
+++ ex10/special.py
@@ -0,0 +1,33 @@
+############################################################
+# FILE : special.py
+# WRITER : shay margolis , roy amir
+# EXERCISE : intro2cs1 ex10 2018-2019
+# DESCRIPTION : A class representing the special shot
+#############################################################
+
+from element import Element
+
+
+class Special(Element):
+    SPECIAL = 4
+    TORPEDO_LIFE_TIME = 200
+
+    def __init__(self, position, velocity, angle, ttl=TORPEDO_LIFE_TIME):
+        """
+        Creates a torpedo class instance
+        :param position: position vector (x,y)
+        :param velocity: velocity vector (v_x, v_y)
+        :param angle: angle in degrees
+        :return:
+        """
+        self.__ttl = ttl
+        Element.__init__(self, position, velocity, angle)
+
+    def get_size(self):
+        return self.TORPEDO_SIZE
+
+    def get_life_time(self):
+        return self.__ttl
+
+    def decrease_life_time(self):
+        self.__ttl -= 1
